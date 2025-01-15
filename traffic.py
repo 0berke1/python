7f2efe64c433233730f2473c3f589318e5893040
0berke1 <118024377+0berke1@users.noreply.github.com>
2025-01-15 12:27:21 +0300

    Update traffic.py
    -math module added to use ceil() function to deal with float nums.
    -new user inputs added : capacitiy of each model and speed limit in addition to number of people. 
    
    -this version now allows users to do calculations with any car capacity and any speed limit.

--- traffic.py
+++ traffic.py
@@ -1,35 +1,38 @@
-#define the max value of speed of cars model A and model B.
-MAX_SPEED_A = 90
-MAX_SPPED_B = 90
+import math
 
-#define the capacitiy of the cars model A and model B
-CAPACITY_A = 5
-CAPACITY_B = 10
+#define the number of people that's gonna be on the road.
+number_of_people = 0
+
 
 
 def main():
 
     print("welcome to traffic calculator. There are two types of cars : Model A and model B")
-    print(f"model A cars have capacity of {CAPACITY_A}.")
-    print(f"model B cars have capacity of {CAPACITY_B}.")
-    print("both models go at the speed of 90 when there're no more than 10 cars on the road.")
+    CAPACITY_A = int(input("enter capacity for model A cars:"))    
+    CAPACITY_B = int(input("enter capacity for model B cars:"))
+    MAX_SPEED = int(input("enter the speed limit:"))
+    print(f"both models go at the speed of {MAX_SPEED} when there're no more than 10 cars on the road.")
 
 
     number_of_people = int(input("how many people will be on the road? "))
 
-    numberofcarsA , numberofcarsB = numberofcars(number_of_people) #calculate how many cars are needed for each model to carry the given number of people.
+    numberofcarsA , numberofcarsB = numberofcars(number_of_people, CAPACITY_A, CAPACITY_B) #calculate how many cars are needed for each model to carry the given number of people.
 
-    model = input("choose a model, A or B ")
+    model = input("choose a model, A or B ").capitalize()
 
     if model == "A":
-        speed = speedofcars(numberofcarsA)
+        speed = speedofcars(numberofcarsA, MAX_SPEED)
+        print(f"{numberofcarsA} model A cars are needed to carry {number_of_people} people.")
+
 
     elif model =="B":
-        speed = speedofcars(numberofcarsB)
+        speed = speedofcars(numberofcarsB, MAX_SPEED)
+        print(f"{numberofcarsB} model B cars are needed to carry {number_of_people} people.")
 
 
-    print(f"the cars has to go at the speed {speed}. They have to slow down {MAX_SPEED_A - speed}. ")
 
+    print(f"the cars has to go at the speed {speed}. They have to slow down {MAX_SPEED - speed}. ")
+   
 
 
 
@@ -37,23 +40,24 @@ def main():
 
 
 #define the function that calculates how many cars of model A and model B are needed to transport a given number of people.
-def numberofcars(number_of_people):
-    #calculate the number of cars of model A and model B needed to transport a given number of people
-    #return the number of cars of model A and model B needed to transport a given number of people
-    number_of_cars_A = number_of_people // CAPACITY_A
-    number_of_cars_B = number_of_people // CAPACITY_B
-    return number_of_cars_A, number_of_cars_B
+def numberofcars(number_of_people , capacitiyA, capacityB):
+
+    number_of_cars_A= number_of_people / capacitiyA
+    number_of_cars_B = number_of_people / capacityB
+
+    return math.ceil(number_of_cars_A), math.ceil(number_of_cars_B)
 
 #define the function that calculates the speed of which the cars on the road has to go given the number of cars
-def speedofcars(number_of_cars):
+def speedofcars(number_of_cars, max_speed):
+
+    change_of_speed = 2 #this variable determines the change ratio of the max speed per car.
 
     if number_of_cars <= 10:
-        speed = 90
+        speed_local = max_speed
     elif number_of_cars > 10:
-        speed = 90 - 2 * number_of_cars
+        speed_local = max_speed - change_of_speed * (number_of_cars - 10)
 
-
-    return speed
+    return speed_local
 
 
 
