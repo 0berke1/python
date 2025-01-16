28816196530553a59d24cd7fbd85335ea0b89dc1
0berke1 <118024377+0berke1@users.noreply.github.com>
2025-01-16 14:26:15 +0300

    Update traffic.py
    each user input is now a global variable. This makes it easier to deal with the function parameters as the global variables can be used in functions without needing a parameter.
    
    new user input: Number of roads. 
    
    speed calcs can be done for mulitple roads.

--- traffic.py
+++ traffic.py
@@ -1,64 +1,74 @@
 import math
 
-#define the number of people that's gonna be on the road.
-number_of_people = 0
-
-
+#global variables. User inputs.
+global SPEED_LIMIT
+global CAPACITY_A
+global CAPACITY_B
+global NUMBER_OF_PEOPLE
+global NUMBER_OF_ROADS 
 
 def main():
 
-    print("welcome to traffic calculator. There are two types of cars : Model A and model B")
-    CAPACITY_A = int(input("enter capacity for model A cars:"))    
-    CAPACITY_B = int(input("enter capacity for model B cars:"))
-    MAX_SPEED = int(input("enter the speed limit:"))
-    print(f"both models go at the speed of {MAX_SPEED} when there're no more than 10 cars on the road.")
+    print("welcome to traffic calculator. There are two types of cars : Model A and model B") # print welcome message.
 
+    #get the capacitiy of the models 
+    global CAPACITY_A 
+    CAPACITY_A = int(input("enter capacity for model A cars:"))                             
 
-    number_of_people = int(input("how many people will be on the road? "))
+    global CAPACITY_B
+    CAPACITY_B = int(input("enter capacity for model B cars:")) 
 
-    numberofcarsA , numberofcarsB = numberofcars(number_of_people, CAPACITY_A, CAPACITY_B) #calculate how many cars are needed for each model to carry the given number of people.
+    #speed limit                                                                                     
+    global SPEED_LIMIT 
+    SPEED_LIMIT = int(input("enter the speed limit:")) 
 
-    model = input("choose a model, A or B ").capitalize()
+    #number of roads
+    global NUMBER_OF_ROADS
+    NUMBER_OF_ROADS = int(input("enter the number of roads:"))                                      
+
+    print(f"both models go at the speed of {SPEED_LIMIT} when there're no more than 10 cars on a single road.")
+
+    #number of people
+    global NUMBER_OF_PEOPLE
+    NUMBER_OF_PEOPLE = int(input("how many people will be on the road? "))
+
+    numberofcarsA, numberofcarsB = numberofcars(NUMBER_OF_PEOPLE) #calculate how many cars are needed for each model to carry the given number of people.
+
+    model = input("choose a model, A or B ").capitalize()  #user chooses a model. Speed gets updated for the chosen model and returned.
 
     if model == "A":
-        speed = speedofcars(numberofcarsA, MAX_SPEED)
-        print(f"{numberofcarsA} model A cars are needed to carry {number_of_people} people.")
+        speed = speedofcars(numberofcarsA)
+        print(f"{numberofcarsA} model A cars are needed to carry {NUMBER_OF_PEOPLE} people.")
 
 
     elif model =="B":
-        speed = speedofcars(numberofcarsB, MAX_SPEED)
-        print(f"{numberofcarsB} model B cars are needed to carry {number_of_people} people.")
+        speed = speedofcars(numberofcarsB)
+        print(f"{numberofcarsB} model B cars are needed to carry {NUMBER_OF_PEOPLE} people.")
 
 
+    print(f"the cars has to go at the speed {speed}. They have to slow down {SPEED_LIMIT - speed}. ")  #new speed limit is shown to the screen.
 
-    print(f"the cars has to go at the speed {speed}. They have to slow down {MAX_SPEED - speed}. ")
-   
-
-
-
-    
 
 
 #define the function that calculates how many cars of model A and model B are needed to transport a given number of people.
-def numberofcars(number_of_people , capacitiyA, capacityB):
+def numberofcars(number_of_people):
 
-    number_of_cars_A= number_of_people / capacitiyA
-    number_of_cars_B = number_of_people / capacityB
+    number_of_cars_A= number_of_people / CAPACITY_A
+    number_of_cars_B = number_of_people / CAPACITY_B
 
     return math.ceil(number_of_cars_A), math.ceil(number_of_cars_B)
 
+
 #define the function that calculates the speed of which the cars on the road has to go given the number of cars
-def speedofcars(number_of_cars, max_speed):
+def speedofcars(number_of_cars):
 
     change_of_speed = 2 #this variable determines the change ratio of the max speed per car.
 
-    if number_of_cars <= 10:
-        speed_local = max_speed
-    elif number_of_cars > 10:
-        speed_local = max_speed - change_of_speed * (number_of_cars - 10)
+    if number_of_cars <= 10 * NUMBER_OF_ROADS:  # this checks if each road has at least 10 cars. If not, each car can go at the speed limit. 
+        speed = SPEED_LIMIT
+    elif number_of_cars > 10 * NUMBER_OF_ROADS: # if each road is filled with at least 10 cars, then they will start slowing down.
+        speed = SPEED_LIMIT - change_of_speed * (number_of_cars - 10 * NUMBER_OF_ROADS)
 
-    return speed_local
-
-
+    return speed
 
 main()
